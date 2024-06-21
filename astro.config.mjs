5c28b644cc2d8f98623911ff2ac3fa542fdd3017
yashtyagi-cjg <s2014.moose@gmail.com>
2024-06-21 22:45:07 +0530

    Reverting npx astro add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,4 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: vercel()
 });
